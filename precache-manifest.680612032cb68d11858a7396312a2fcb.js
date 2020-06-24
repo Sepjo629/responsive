71479cd038b6f396a66eb1f71d3c2ea4ea025124
Sepjo <chlgud629@gmail.com>
2020-06-25 01:50:48 +1200

    Updates

---
+++ precache-manifest.680612032cb68d11858a7396312a2fcb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8c60a128a2db20b8a903086b6da62f",
+    "url": "/responsive/index.html"
+  },
+  {
+    "revision": "063b1ddaa2ec0f59237f",
+    "url": "/responsive/static/css/main.c74ec7e0.chunk.css"
+  },
+  {
+    "revision": "ce7a3b0462cef2f475cd",
+    "url": "/responsive/static/js/2.268cf6e4.chunk.js"
+  },
+  {
+    "revision": "baebdd1671010f55d6a3b55cb359d81e",
+    "url": "/responsive/static/js/2.268cf6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "063b1ddaa2ec0f59237f",
+    "url": "/responsive/static/js/main.c2956795.chunk.js"
+  },
+  {
+    "revision": "7a3a5c282e4e524ab65c",
+    "url": "/responsive/static/js/runtime-main.14385b24.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/responsive/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/responsive/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/responsive/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/responsive/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "24e8d8f29296438b099443772c822a1f",
+    "url": "/responsive/static/media/up-arrow-circle.24e8d8f2.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/responsive/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
